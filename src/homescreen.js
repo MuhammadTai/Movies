dbb76d36ed822346a7406f51f2dc0da7dc2f7a90
muhammadtai <mohammedtai1@hotmail.co.uk>
2019-08-07 22:23:06 +0100

    This is my 3rd commit!

--- src/homescreen.js
+++ src/homescreen.js
@@ -6,10 +6,11 @@ class Home extends React.Component {
     constructor(props) {
       super(props);
       this.fetchData = this.fetchData.bind(this);
-      this.defaultscreen = this.defaultscreen.bind(this);
-      this.state = {movies: ''};
+      //this.defaultscreen = this.defaultscreen.bind(this);
+      this.state = {movies: []};
       this.movies = [];
       this.defaultMovies= [];
+
     }
    
     //get the data at the start 
@@ -25,19 +26,22 @@ class Home extends React.Component {
         }
     }
 
-    async defaultscreen(){
+    async componentDidMount(){
         try {
                 const responsearray=[`http://www.omdbapi.com/?apikey=f94e8b75&i=tt0111161`,
-                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0111161`,
-                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0111161`,
-                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0111161`,
-                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0111161`];
+                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0068646`,
+                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0468569`,
+                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0071562`,
+                `http://www.omdbapi.com/?apikey=f94e8b75&i=tt0167260`];
                 //promise all ensures all the async functions passed are resolved and returns a single promise
                 await Promise.all(responsearray.map((response)=> fetch(response)
                             .then((response) => response.json())
                             .then((movies) => this.defaultMovies.push(movies))))
             
                 console.log(this.defaultMovies);
+                this.defaultMovies.sort(function(a, b){return b.imdbRating - a.imdbRating})
+                this.setState({movies: this.defaultMovies})
+
             }
             catch (error) {
                 console.error(error);
@@ -45,16 +49,17 @@ class Home extends React.Component {
     }
     
     render() {
+
         const searched = this.props.search;
         const regexConst = /^ /;
         const listMovies = this.defaultMovies.map((movie) =>
-        <li className="inline-item">
-            <div className="card" style="width:400px">
-                <img className="card-img-top" src={movie.poster} alt="Movie" style="width:100%"/>
-                <div className="card-body">
-                <h4 className="card-title">{movie.title}</h4>
-                <p className="card-text">{movie.description}</p>
-                <a href="/" className="btn btn-primary">More Detail</a>
+        <li className="list-inline-item .justify-content-*-center padding" key={movie.imdbID}>
+            <div className="card cardw">
+                <img className="card-img-top cardw1" src={movie.Poster} alt="Movie"/>
+                <div className="card-body cardb">
+                    <h4 className="card-title">{movie.Title} ({movie.imdbRating})</h4>
+                    <p className="card-text">{movie.Plot}</p>
+                    <a href="/" className="btn btn-primary">More Detail</a>
                 </div>
             </div>
         </li>
@@ -68,18 +73,20 @@ class Home extends React.Component {
                 </div>
             );
         }
-        else{
-            this.defaultscreen();
+        else {
+            //this.defaultscreen();
+            
+                console.log(this.state.movies)
+                return (
+                    <div>
+                        <p className="search-title">Highest Rated Movies</p>
+                        <ul className="list-inline">
+                            {listMovies}
+                        </ul>
 
-            return (
-                <div>
-                    <p className="search-title">Highest Rated Movies</p>
-                    <ul className="list-inline">
-                        {listMovies}
-                    </ul>
-
-                </div>
-            );
+                    </div>
+                );
+            
         }
     }
   }

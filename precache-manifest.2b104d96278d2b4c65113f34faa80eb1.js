52d6cfa72dfb37cdbb669d22a0a4a239bce7bb26
muhammadtai <mohammedtai1@hotmail.co.uk>
2019-08-14 23:23:59 +0100

    Updates

---
+++ precache-manifest.2b104d96278d2b4c65113f34faa80eb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7387fe0564269c380f98bfe59292c4",
+    "url": "/Movies/index.html"
+  },
+  {
+    "revision": "9d41acc56527543367df",
+    "url": "/Movies/static/css/main.4c0f1651.chunk.css"
+  },
+  {
+    "revision": "1ea2948ff1ce7d418c9f",
+    "url": "/Movies/static/js/2.83347dcd.chunk.js"
+  },
+  {
+    "revision": "9d41acc56527543367df",
+    "url": "/Movies/static/js/main.0a8997dc.chunk.js"
+  },
+  {
+    "revision": "ff7444c216c0f17534ff",
+    "url": "/Movies/static/js/runtime~main.a0dd656c.js"
+  },
+  {
+    "revision": "493743b72d1424224b77b858d1647822",
+    "url": "/Movies/static/media/2000px-MarvelLogo.493743b7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Movies/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ecd0b5d87a1ed8a2b1a5616b0cbfb976",
+    "url": "/Movies/static/media/matrix.ecd0b5d8.jpg"
+  },
+  {
+    "revision": "35811044de3e7e7d0a6563f1cb10f31c",
+    "url": "/Movies/static/media/star_wars_logo-svg_.35811044.png"
+  },
+  {
+    "revision": "e267b2944ecd9c275f2bd5e5390006ac",
+    "url": "/Movies/static/media/wp1945898-movie-posters-wallpapers.e267b294.jpg"
+  }
+]);
